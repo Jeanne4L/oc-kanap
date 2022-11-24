83d7e5d5303ba0af5073fa9881194b6aa80afea4
Jeanne4L <100489924+Jeanne4L@users.noreply.github.com>
2022-11-24 13:33:47 +0100

    Amélioration du code et correction des regex

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,40 +1,32 @@
-//
-//        GET INFORMATION FROM PRODUCT TO CART AND DISPLAY ITS
-// 
-
 // if cart exists, display it
 let cart = [];
 cart = JSON.parse(localStorage.getItem('cart')); 
 if (cart !== null) {
-    for(let i= 0; i<cart.length; i++) {    
-        displayCart(cart[i].id, cart[i].color, cart[i].image, cart[i].altText, cart[i].name, cart[i].price, cart[i].quantity);  
-    }
-    calcTotal()
+    displayCart();  
+    calcTotalPrice()
+    changeQty()
+    deleteProduct()
 }
-changeQty()
-deleteProduct()
 
-// request to get information whose product corresponds to id
-fetch('http://localhost:3000/api/products')
-.then(function(response) {
+// get product information in API
+async function fetchProductsInApi() {
+    const response = await fetch('http://localhost:3000/api/products')
     if(response.ok) {
         return response.json();
     }
-})
-.then(function(products) {
-    getProductData(products);
-})
+    throw new Error('Impossible d\'accéder au serveur')
+}
+fetchProductsInApi().then(products => getProductData(products))
 
-// gather products information from API and localStorage
+// compare and gather product information
 function getProductData(products) {
     let productData = JSON.parse(localStorage.getItem('productData'));
 
     if(productData !== null) {
         for (i=0; i<productData.length; i++) {
-            var product = productData[i];
-            var productId = product.id;
-            var productColor = product.color;
-            var productQty = Number(product.quantity); 
+            var productId = productData[i].id;
+            var productColor = productData[i].color;
+            var productQty = Number(productData[i].quantity); 
     
             for (var j =0; j<products.length; j++) {
                 var productName = products[j].name;
@@ -48,7 +40,7 @@ function getProductData(products) {
             }
         } 
     }
-}
+}        
 
 // if exists get cart in localStorage and add new products
 function getCart(id, color, qty, name, price, img, altText) {
@@ -56,8 +48,8 @@ function getCart(id, color, qty, name, price, img, altText) {
     if(cart) {  
         document.querySelector('#cart__items').innerHTML = ''; 
 
+        //get existing row index 
         for(let i= 0; i<cart.length; i++) { 
-            //get existing row index 
             if(cart[i].id == id && cart[i].color == color) {
                 var index = cart.indexOf(cart[i]);
                 break;
@@ -65,42 +57,23 @@ function getCart(id, color, qty, name, price, img, altText) {
                 var index = -1;
             }
         }
-        // problème addition prix
+
         if(index !== null && index !== -1 && cart.length !== 0) {
             cart[index].price = price * (cart[index].quantity += qty)
         } else {
-            cart.push({
-                id: id, 
-                color: color,
-                quantity: qty,
-                name: name, 
-                price: price * qty,
-                image: img, 
-                altText: altText
-            });
+            pushProduct(id, color, qty, name, price, img, altText)
         }
         localStorage.setItem('cart', JSON.stringify(cart.sort(sorting)));
         localStorage.removeItem('productData');
 
     } else {
         cart = [];
-        cart.push({
-            id: id, 
-            color: color,
-            quantity: qty,
-            name: name, 
-            price: price * qty,
-            image: img, 
-            altText: altText
-        });
+        pushProduct()
         localStorage.setItem('cart', JSON.stringify(cart));
         localStorage.removeItem('productData');
     }
-
-    for(let i= 0; i<cart.length; i++) {   
-        displayCart(cart[i].id, cart[i].color, cart[i].image, cart[i].altText, cart[i].name, cart[i].price, cart[i].quantity); 
-    }
-    calcTotal()
+    displayCart(); 
+    calcTotalPrice()
     changeQty()
     deleteProduct()
 }
@@ -110,33 +83,60 @@ function sorting(a,b) {
     return a.name > b.name ? 1 : -1;
 };
 
-function displayCart (id, color, image, alt, name, price, quantity){
-    document.querySelector('#cart__items').innerHTML += 
-    '<article class="cart__item" data-id="'+id+'" data-color="'+color+'">'+
-        '<div class="cart__item__img">'+
-            '<img src="'+image+'" alt="'+alt+'">'+
-        '</div>'+
-        '<div class="cart__item__content">'+
-            '<div class="cart__item__content__description">'+
-                '<h2>'+name+'</h2>'+
-                '<p>'+color+'</p>'+
-                '<p>'+price+' €</p>'+
+// push new product
+function pushProduct(id, color, qty, name, price, img, altText) {
+    cart.push({
+        id: id, 
+        color: color,
+        quantity: qty,
+        name: name, 
+        price: price * qty,
+        image: img, 
+        altText: altText
+    });
+}
+
+function displayCart(){
+    for(let i=0; i<cart.length; i++) {
+        document.querySelector('#cart__items').innerHTML += 
+        '<article class="cart__item" data-id="'+cart[i].id+'" data-color="'+cart[i].color+'">'+
+            '<div class="cart__item__img">'+
+                '<img src="'+cart[i].image+'" alt="'+cart[i].altText+'">'+
             '</div>'+
-            '<div class="cart__item__content__settings">'+
-                '<div class="cart__item__content__settings__quantity">'+
-                    '<p>Qté : </p>'+
-                    '<input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="'+quantity+'">'+
+            '<div class="cart__item__content">'+
+                '<div class="cart__item__content__description">'+
+                    '<h2>'+cart[i].name+'</h2>'+
+                    '<p>'+cart[i].color+'</p>'+
+                    '<p>'+cart[i].price+' €</p>'+
                 '</div>'+
-                '<div class="cart__item__content__settings__delete">'+
-                    '<p class="deleteItem">Supprimer</p>'+
-                '</div'+
+                '<div class="cart__item__content__settings">'+
+                    '<div class="cart__item__content__settings__quantity">'+
+                        '<p>Qté : </p>'+
+                        '<input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="'+cart[i].quantity+'">'+
+                    '</div>'+
+                    '<div class="cart__item__content__settings__delete">'+
+                        '<p class="deleteItem">Supprimer</p>'+
+                    '</div'+
+                '</div>'+
             '</div>'+
-        '</div>'+
-    '</article>';
+        '</article>';
+    }
+}
+
+function calcTotalPrice() {
+    let totalPrice = 0;
+    let totalQty = 0;
+
+    for( let i=0; i<cart.length; i++) {
+        totalPrice += cart[i].price
+        totalQty += cart[i].quantity
+    }
+    document.querySelector('#totalQuantity').textContent = totalQty
+    document.querySelector('#totalPrice').textContent = totalPrice
 }
 
 //
-//        GET PRODUCT CHANGES FROM CART PAGE
+//        WATCH FOR CHANGES
 // 
 
 function changeQty() {
@@ -154,7 +154,7 @@ function changeQty() {
             
             for (let j =0; j<cart.length; j++) {
                 if(cart[j].id == datasetId && cart[j].color == datasetColor) {
-                    
+
                     cart[j].price /= cart[j].quantity 
                     cart[j].quantity = inputValue
                     if(inputValue == 0 || inputValue == null) {
@@ -166,7 +166,7 @@ function changeQty() {
                     priceDisplay.textContent = cart[j].price + ' €';
                     localStorage.setItem('cart', JSON.stringify(cart));
                 }
-                calcTotal()
+                calcTotalPrice()
             }
         })
     }
@@ -181,9 +181,6 @@ function deleteProduct() {
             let datasetColor = deleteBtn[i].closest('[data-color]').dataset.color
 
             for (let j=0; j<cart.length; j++) {
-                // if(cart.length == 1) {
-                //     cart = null
-                // }
                 if(cart[j].id == datasetId && cart[j].color == datasetColor) {
                     let index = cart.indexOf(cart[j])
                     cart.splice(index, 1)
@@ -191,69 +188,55 @@ function deleteProduct() {
                 }
             }
             document.querySelector('#cart__items').innerHTML = ''; 
-            for(let i= 0; i<cart.length; i++) {   
-                displayCart(cart[i].id, cart[i].color, cart[i].image, cart[i].altText, cart[i].name, cart[i].price, cart[i].quantity); 
-            }
-            calcTotal()
+            displayCart(); 
+            calcTotalPrice()
+            changeQty()
             deleteProduct()
         })
     }
 }
 
-function calcTotal() {
-    let totalPrice = 0;
-    let totalQty = 0;
+// //
+// //        FORM
+// // 
 
-    for( let i=0; i<cart.length; i++) {
-        totalPrice += cart[i].price
-        totalQty += cart[i].quantity
-    }
-    document.querySelector('#totalQuantity').textContent = totalQty
-    document.querySelector('#totalPrice').textContent = totalPrice
-}
-
-//
-//        FORM
-// 
-
-let letterRegex = /[A-Za-z]/g
-let nbAndLetterRegex = /\w/g
+let letterRegex = /[^A-Za-z]/
+let nbAndLetterRegex = /[^\w\s]/
 let emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-
 let contact = {}
 let products= []
 
 // check input value
-document.querySelector('#firstName').addEventListener('change', (e) => {
+document.querySelector('#firstName').addEventListener('input', (e) => {
     if(letterRegex.test(e.target.value)) {
+        document.querySelector('#firstNameErrorMsg').textContent = 'Ce champ ne doit contenir que des lettres'
+    } else {
         document.querySelector('#firstNameErrorMsg').textContent = ''
         contact.firstName = e.target.value
-    } else {
-        document.querySelector('#firstNameErrorMsg').textContent = 'Vous devez saisir votre prénom'
     }
 })
-document.querySelector('#lastName').addEventListener('change', (e) => {
+document.querySelector('#lastName').addEventListener('input', (e) => {
     if(letterRegex.test(e.target.value)) {
+        document.querySelector('#lastNameErrorMsg').textContent = 'Ce champ ne doit contenir que des lettres'
+    } else {
         document.querySelector('#lastNameErrorMsg').textContent = ''
         contact.lastName = e.target.value
-    } else {
-        document.querySelector('#lastNameErrorMsg').textContent = 'Vous devez saisir votre nom de famille'
     }
 })
-document.querySelector('#address').addEventListener('change', (e) => {
+document.querySelector('#address').addEventListener('input', (e) => {
     if(nbAndLetterRegex.test(e.target.value)) {
+        document.querySelector('#addressErrorMsg').textContent = 'Ce champ ne doit pas contenir de caractères spéciaux'
+    } else {
         document.querySelector('#addressErrorMsg').textContent = ''
         contact.address = e.target.value
-    } else {
-        document.querySelector('#addressErrorMsg').textContent = 'Vous devez saisir votre adresse'
     }
 })
-document.querySelector('#city').addEventListener('change', (e) => {
+document.querySelector('#city').addEventListener('input', (e) => {
     if(letterRegex.test(e.target.value)) {
+        document.querySelector('#cityErrorMsg').textContent = 'Ce champ ne doit contenir que des lettres'
+    } else {
         document.querySelector('#cityErrorMsg').textContent = ''
         contact.city = e.target.value
-    } else {
-        document.querySelector('#cityErrorMsg').textContent = 'Vous devez saisir votre ville'
     }
 })
 document.querySelector('#email').addEventListener('change', (e) => {
@@ -261,23 +244,23 @@ document.querySelector('#email').addEventListener('change', (e) => {
         document.querySelector('#emailErrorMsg').textContent = ''
         contact.email = e.target.value
     } else {
-        document.querySelector('#emailErrorMsg').textContent = 'Email invalide'
+        document.querySelector('#emailErrorMsg').textContent = 'Cet email est invalide'
     }
 })
 
-// submit
+// submit form
 document.querySelector('#order').addEventListener('click', (e) => {
     e.preventDefault()
     for (let i=0; i<cart.length; i++) {
         products.push(cart[i].id)
     }
-    fetchProducts().then(function(order) {
+    postProducts().then(function(order) {
         let orderId= order.orderId
         window.location.href='./confirmation.html?id='+orderId
     })
 })
 
-async function fetchProducts() {
+async function postProducts() {
     const res = await fetch('http://localhost:3000/api/products/order', {
         method: 'POST',
         headers: {
@@ -292,5 +275,5 @@ async function fetchProducts() {
     if (res.ok) {
         return res.json()
     } 
-    throw new Error(res.status)
+    throw new Error('Impossible d\'accéder au serveur')
 }
